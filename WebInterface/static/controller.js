6fa33a4611edae2a1e331bd4827acc9425cc4730
Axel Van Uffelen <axel.van.uffelen1@gmail.com>
2019-03-14 00:01:47 +0100

    Img fix

--- WebInterface/static/controller.js
+++ WebInterface/static/controller.js
@@ -6,9 +6,10 @@ function Lampje(lampje){
     // xhr.open('GET','/lampje'+lampje);
     // xhr.send();
     document.getElementById('lampje').innerText=lampje;
-    document.getElementById('fototje').src=lampje==1?"turbine-1.jpg": "turbine-2.jpg";    
     socket.emit('msg', lampje);
 }
 socket.on("btnpress",function(msg){
+    document.getElementById('fototje').src=msg==1?"turbine-1.jpg": "turbine-2.jpg";    
+    
     document.getElementById('lampje').innerText=msg;
 })

5b38609e7e492ab9036c6c5ddf7f2ecba5ab7164
Axel Van Uffelen <axel.van.uffelen1@gmail.com>
2019-03-13 23:59:29 +0100

    Foto

--- WebInterface/static/controller.js
+++ WebInterface/static/controller.js
@@ -5,7 +5,8 @@ function Lampje(lampje){
     // var xhr=new XMLHttpRequest();
     // xhr.open('GET','/lampje'+lampje);
     // xhr.send();
-    document.getElementById('lampje').innerText=lampje;    
+    document.getElementById('lampje').innerText=lampje;
+    document.getElementById('fototje').src=lampje==1?"turbine-1.jpg": "turbine-2.jpg";    
     socket.emit('msg', lampje);
 }
 socket.on("btnpress",function(msg){
